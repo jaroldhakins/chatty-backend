38b7a257350cdc66e922218379b103ef5b666dc7
unknown <stivenhakins@gmail.com>
2024-06-01 09:54:11 -0500

    setup project

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import expres, { Express } from 'express';
+import { ChattyServer } from './setupServer';
+import databaseConnection from './setupDatabase';
+import { config } from './config';
+
+class Application {
+    public initialize(): void {
+        this.loadConfig();
+        databaseConnection();
+        const app: Express = expres();
+        const server: ChattyServer = new ChattyServer(app);
+        server.start();
+    }
+
+    private loadConfig(): void {
+        config.validateConfig();
+    }
+}
+
+const application: Application = new Application();
+application.initialize();
